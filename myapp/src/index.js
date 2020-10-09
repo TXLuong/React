1aaf33a84fca0e7c867bed363915ba4a4cc58412
TXLuong <tranxuanluong98@gmail.com>
2020-10-09 20:09:08 +0700

    app js

---
+++ myapp/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Login from './components/Login';
+ReactDOM.render(<Login></Login>,
+  document.getElementById('root')
+);
