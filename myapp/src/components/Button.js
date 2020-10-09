0c987877ee158472af9a76474b9e2e8e3c22d4f9
TXLuong <tranxuanluong98@gmail.com>
2020-10-09 20:09:08 +0700

    mot gio roi

---
+++ myapp/src/components/Button.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+class Button extends Component {
+    render() {
+        return (
+            <button ref = "https/profile">
+                Login 
+            </button>
+        );
+    }
+}
+export default Button;
