936a6de6f0a32e17b78abb6bc4920b96fccc12b2
TXLuong <tranxuanluong98@gmail.com>
2020-11-26 00:19:46 +0700

    ối dồi ôi

---
+++ myapp/src/Why.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Why extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1> tai soa lai khong hien thi app </h1>
+            </div>
+        )
+    }
+}
+export default Why;
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Login from './components/Login';
-ReactDOM.render(<Login></Login>,
-  document.getElementById('root')
-);
+import App from './App'
+ReactDOM.render((
+    <App/>, document.getElementById("root")
+));
