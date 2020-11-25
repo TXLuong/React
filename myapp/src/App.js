3378fb53f89aec400ab8132b10080f06b68869f7
TXLuong <tranxuanluong98@gmail.com>
2020-11-26 00:19:46 +0700

    hai gio roi

---
+++ myapp/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+function App(){
+    return <Button>Hello world </Button>
+}
+export default App;
---
+++ myapp/src/components/About.js
@@ -0,0 +1,15 @@
+import { CalendarToday } from '@material-ui/icons';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, Link, browserHistory, IndexRoute} from 'react-router';
+
+class About extends React.Component{
+    render(){
+        <div>
+            <h1>
+                About ... 
+            </h1>
+        </div>
+    }
+}
+export default About;
